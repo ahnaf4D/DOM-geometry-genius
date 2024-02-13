60ee43585425fafb99b5fbef2eb68c30aa59d78f
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-13 21:09:39 +0600

    UI design completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
     theme: {
         extend: {},
     },
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
+    colors: {
+        title_color_first: 'rgba(17, 17, 17, 0.8)',
+        title_color_second: 'rgba(17, 17, 17, 0.6)'
+    },
     plugins: [],
 }
